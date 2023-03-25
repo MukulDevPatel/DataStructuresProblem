1cc09f607d8a623e566d6060c2f22ba7ffc2035a
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-25 21:42:06 +0530

    [Mukul Dev] Add: This is Data Structure problems

---
+++ BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    public class BinarySearch
+    {
+        public void ReadFile(string filePath)
+        {
+            string readData = File.ReadAllText(filePath);
+            string[] words = readData.Split(" ");
+            foreach (var data in words)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("Enter word to search");
+            string search = Console.ReadLine();
+            foreach (var data in words)
+            {
+                if (data.Equals(search))
+                {
+                    Console.WriteLine("Found");
+                    return;
+                }
+            }
+            Console.WriteLine("Not found");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataStructureProblem
+{
+    class Problem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Data Structure Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the number for execution");
+                Console.WriteLine("\n1. Binary Search\n2. Exit");
+                int select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        BinarySearch binary = new BinarySearch();
+                        binary.ReadFile(@"D:\BridgeLabz\DataStructuresProblem\File.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
