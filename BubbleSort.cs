35fef4bb309ea3d9de7b966b5e2e1b6645f22bca
MukulDevPatel <patelmukuldev@gmail.com>
2023-03-26 17:49:23 +0530

    [Mukul Dev] Add: This is bubble sorting

---
+++ BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    public class BubbleSort
+    {
+        public void ReadFile(string filePath)
+        {
+            string readData = File.ReadAllText(filePath);
+            string[] words = readData.Split(" ");
+            int[] arr = new int[words.Length];
+            int count = 0;
+            foreach (var data in words)
+            {
+                arr[count] = Convert.ToInt32(Console.ReadLine());
+                count++;
+                Console.WriteLine(data);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i;  j < arr.Length - 1; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (var data in arr)
+            {
+                Console.WriteLine("Sorted List: "+ data + " ");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace DataStructureProblem
             while (flag)
             {
                 Console.WriteLine("Enter the number for execution");
-                Console.WriteLine("\n1. Binary Search\n2. Exit");
+                Console.WriteLine("\n1. Binary Search\n2. BubbleSort\n3. Exit");
                 int select = Convert.ToInt32(Console.ReadLine());
                 switch (select)
                 {
@@ -20,6 +20,10 @@ namespace DataStructureProblem
                         binary.ReadFile(@"D:\BridgeLabz\DataStructuresProblem\File.txt");
                         break;
                     case 2:
+                        BubbleSort bubble = new BubbleSort();
+                        bubble.ReadFile(@"D:\BridgeLabz\DataStructuresProblem\FileInteger.txt");
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
